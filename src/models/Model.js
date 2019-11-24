b35f6ef141208e70b53a50d07413d8c02b00357c
Vinci Taylaran <p.taylaran792@mybvc.ca>
2019-11-23 18:15:45 -0700

    firebase removed, added filter and user creation functionality in Model.

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,38 +1,43 @@
 import { observable } from "mobx";
 
-import firebase from "../firebase";
-import "firebase/database";
+/*
+TODO:
+- create models for service workers and clients.
+*/
 
 export const Model = observable({ test: true });
 
-Model.ListUsers = async function() {
-  let users = [];
+// Converts a time string (24-hour format) to its equivalent value in seconds.
+// You can use the output of this method to check if one the worker's time blocks
+// are greater than or equal to a client's search criteria.
+Model.TimeToSeconds = function(time) {
+  // Turns time string to an array of substrings.
+  let array = time.split(":");
 
-  firebase
-    .firestore()
-    .collection("users")
-    .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => users.push(doc.data()));
-    });
+  // Converts each string in array to number.
+  let seconds = +array[0] * 60 * 60 + +array[1] * 60 + +array[2];
 
-  return await users;
+  return seconds;
 };
 
-Model.CreateUser = function() {
-  let database = firebase.firestore();
-  database
-    .collection("users")
-    .doc()
-    .set({ email: "sample5@email.com" });
-};
+// ATTENTION: availabilityBlock must be passed an OBJECT as an argument! Below is the schema.
+Model.List = async function(availabilityBlock = undefined) {
+  /*************************** 
+  SCHEMA for availabilityBlock
+  {
+    day: string,
+    start: string(24-h format),
+    end: string(24-h format)
+  }
+  ***************************/
+  let fetchData, reply, data, users;
+  let filteredUsers = [];
 
-Model.GetFromAwsBucket = async function() {
   try {
-    const PutUrl =
-      "https://bvc-vincibucket.s3.ca-central-1.amazonaws.com/sampledata.json";
+    const Endpoint =
+      "https://bvc-vincibucket.s3.ca-central-1.amazonaws.com/mydata.json";
 
-    let FetchData = {
+    fetchData = {
       method: "GET",
       mode: "cors",
       cache: "no-cache",
@@ -44,26 +49,93 @@ Model.GetFromAwsBucket = async function() {
       }
     };
 
-    const reply = await fetch(PutUrl, FetchData);
-    const data = await reply.json();
+    reply = await fetch(Endpoint, fetchData);
+    data = await reply.json();
 
-    console.log(data);
+    // Assigns a deep copy of the fetched data to 'users'.
+    users = JSON.parse(JSON.stringify(data));
+
+    // If no object was passed as an argument, list all users.
+    if (availabilityBlock !== undefined) {
+      const startTime = this.TimeToSeconds(availabilityBlock.startTime);
+      const endTime = this.TimeToSeconds(availabilityBlock.endTime);
+
+      // Filters users whose availability matches to what was passed an an arguemnt
+      // to availabilityBlock.
+      filteredUsers = users.filter(user => {
+        return user.availability.some(available => {
+          return (
+            available.day === availabilityBlock.day &&
+            (this.TimeToSeconds(available.start) >= startTime ||
+              this.TimeToSeconds(available.end) <= endTime)
+          );
+        });
+      });
+
+      console.log(filteredUsers);
+      return filteredUsers;
+    } else {
+      console.log(data);
+      return data;
+    }
   } catch (e) {
-    console.error(`Error: ${e}`);
+    console.log("[List] Exception! e -->");
+    console.log(e);
   }
 };
 
-Model.PutToAwsBucket = async function() {
+Model.CreateUser = async function(user) {
+  /***************************
+   SCHEMA for service worker user
+  {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    phoneNumber: string,
+    aboutMe: string,
+    availability: [ 
+      {
+        day: string,
+        start: string (time, 24-h format),
+        end: string (time, 24-h format)
+      }
+    ]
+  } 
+  ***************************/
+  let reply, data, fetchData, users;
+
   try {
-    // Endpoint on AWS.  Change "your-bucket-name" to ... you can guess.
-    const PutUrl =
-      "https://bvc-vincibucket.s3.ca-central-1.amazonaws.com/sampledata.json"; // + ".json";
+    const Endpoint =
+      "https://bvc-vincibucket.s3.ca-central-1.amazonaws.com/mydata.json"; // + ".json";
+
+    // First make a GET request to get the current array of employee objects.
+    fetchData = {
+      method: "GET",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "omit",
+      headers: {
+        "Content-Type": "application/json",
+        "x-amz-date": new Date().toUTCString(),
+        "x-amz-acl": "public-read"
+      }
+    };
+
+    reply = await fetch(Endpoint, fetchData);
+    data = await reply.json();
+
+    // Use deep copying on the JSON data that was requested.
+    users = JSON.parse(JSON.stringify(data));
+
+    // Add to the current deep copy of the data that was retrieved.
+    users.push(user);
+
     // Convert the payload to JSON.
-    const ObjectToStoreInJSON = JSON.stringify({
-      test: true
-    });
+    const ObjectToStoreInJSON = JSON.stringify(users);
+
     // Build out the request body.  The x-amz-* headers are required by AWS.
-    let FetchData = {
+    let PutData = {
       method: "PUT",
       mode: "cors",
       cache: "no-cache",
@@ -78,15 +150,76 @@ Model.PutToAwsBucket = async function() {
     };
     // Start the async call.
     console.log("[validation-page.js] Preparing to fetch/put, payload -->");
-    console.log(PutUrl);
-    console.log(FetchData);
-    let FetchReply = await fetch(PutUrl, FetchData);
+    console.log(Endpoint);
+    console.log(PutData);
+    let Fetchreply = await fetch(Endpoint, PutData);
     console.log("Heard back from fetch/put --> ");
-    console.log(FetchReply);
+    console.log(Fetchreply);
     // Assess.
-    return FetchReply && FetchReply.status === 200;
+    return Fetchreply && Fetchreply.status === 200;
   } catch (e) {
-    console.log("[PutToAwsBucket] Exception! e -->");
+    console.log("[CreateUser] Exception! e -->");
+    console.log(e);
+  }
+};
+
+// FIXME: This method is a work in progress.
+Model.DeleteUser = async function(email) {
+  let reply, data, fetchData, users;
+
+  try {
+    const Endpoint =
+      "https://bvc-vincibucket.s3.ca-central-1.amazonaws.com/mydata.json"; // + ".json";
+
+    // First make a GET request to get the current array of employee objects.
+    fetchData = {
+      method: "PUT",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "omit",
+      headers: {
+        "Content-Type": "application/json",
+        "x-amz-date": new Date().toUTCString(),
+        "x-amz-acl": "public-read"
+      }
+    };
+
+    reply = await fetch(Endpoint, fetchData);
+    data = await reply.json();
+
+    // Use deep copying on the JSON data that was requested.
+    users = JSON.parse(JSON.stringify(data));
+
+    users = users.filter(user => user.email !== email);
+
+    // Convert the payload to JSON.
+    const ObjectToStoreInJSON = JSON.stringify(users);
+
+    // Build out the request body.  The x-amz-* headers are required by AWS.
+    let PutData = {
+      method: "PUT",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "omit",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": ObjectToStoreInJSON.length,
+        "x-amz-date": new Date().toUTCString(),
+        "x-amz-acl": "public-read"
+      },
+      body: ObjectToStoreInJSON
+    };
+    // Start the async call.
+    console.log("[validation-page.js] Preparing to fetch/put, payload -->");
+    console.log(Endpoint);
+    console.log(PutData);
+    let Fetchreply = await fetch(Endpoint, PutData);
+    console.log("Heard back from fetch/put --> ");
+    console.log(Fetchreply);
+    // Assess.
+    return Fetchreply && Fetchreply.status === 200;
+  } catch (e) {
+    console.log("[DeleteUser] Exception! e -->");
     console.log(e);
   }
 };
